604767a8358c6b41e9cf5e8db498a1d5fa84c8c6
katerynachenina@gmail.com <katerynachenina@gmail.com>
2020-10-28 13:50:11 +0200

    all works

---
+++ py_setup.R
@@ -0,0 +1,8 @@
+library(reticulate)
+use_condaenv("C:/Users/Pavel/anaconda3/envs/api-env", required = TRUE)
+
+
+#pip install -U scikit-learn
+#pip install Flask
+#pip install flask-restful
+#pip freeze > requirements.txt  
